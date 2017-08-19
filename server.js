b2a82b059820e5fa8001ce1fa043ebd10c990e94
Sourav07 <sourav.nayak111@gmail.com>
2017-08-19 15:35:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req, res){
-   res.send("Article one will be created and served") 
+   res.send("Article one will be created and served");
 });
 
 app.get('/article-two', function(req, res){
-   res.send("Article two will be created and served") 
+   res.send("Article two will be created and served");
 });
 
 app.get('/article-three', function(req, res){
-   res.send("Article three will be created and served") 
+   res.send("Article three will be created and served");
 });
 
 app.get('/ui/style.css', function (req, res) {
