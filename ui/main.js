d958f3a9ef44a11387423477394800a8254bee45
Sourav07 <sourav.nayak111@gmail.com>
2017-08-19 18:19:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 //Changing the text content
 var element = document.getElementById("main-text");
 element.innerHTML = "NewValue";
+
+//Move the image
+var imgElement = document.getElementById("madi");
+imgElement.onClick = function(){
+    imgElement.style.marginLeft = "100px";
+};
