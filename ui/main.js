48a4593093b3b4747e2ca206aa48150f831d013a
Sourav07 <sourav.nayak111@gmail.com>
2017-08-19 19:27:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,16 @@ console.log('Loaded!');
 var counter = 0;
 var button = document.getElementById("counter");
 button.onclick = function(){
-    counter = counter + 1;
-    var countElement = document.getElementById("count");
-    countElement.innerHTML = counter.toString();
+    var request = new XMLHttpRequest();
+    request.onreadystatechanged = function(){
+        if(request.readyState == XMLHttpRequest.DONE){
+            if(request.status == 200){
+                var counter = request.responseText;
+                var countElement = document.getElementById("count");
+                countElement.innerHTML = counter.toString();
+            }
+        }
+    request.open('GET', "http://souravnayak111.imad.hasura-app.io/counter", true);
+    request.send(null);
+    };
 };
