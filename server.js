ffd8c2000fac435ef69c79d307a5fa23e050ab66
Sourav07 <sourav.nayak111@gmail.com>
2017-08-19 17:48:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,15 +94,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function(req, res){
   var articleName = req.params.articleName;    
-  res.send(createTemplate(article[articleName]));
-});
-
-app.get('/article-two', function(req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function(req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
