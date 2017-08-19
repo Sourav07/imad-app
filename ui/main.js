d8240513e02305668a10fb1db1ba4391f8153499
Sourav07 <sourav.nayak111@gmail.com>
2017-08-19 21:59:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,8 +27,9 @@ submitButton.onclick = function(){
         if(request.readyState == XMLHttpRequest.DONE){
             if(request.status == 200){
                 var names = request.responseText;
-                names = JSON.parse(names);
                 console.log(names);
+                names = JSON.parse(names);
+                
                 for(var i = 0; i < names.length; i++){
                     list += "<li>"+names[i]+"</li>";
                 }
