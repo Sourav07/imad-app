3a0cab469c3496011c58e6ab74842f896f1ca78b
Sourav07 <sourav.nayak111@gmail.com>
2017-08-27 18:24:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 var config = {
   user:"souravnayak111",
   database:"souravnayak111",
-  host:"http://db.imad.hasura-app.io",
+  host:"db.imad.hasura-app.io",
   port:"5432",
   password:process.env.DB_PASSWORD
 };
